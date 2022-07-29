f8bb620f5127edfdccbdb206a76047a8ae3e14b6
cmluna2913 <cmluna2913@gmail.com>
2022-07-28 21:51:11 -0500

    Updated etl functions and included automatic notes

--- src/etl_functions.py
+++ src/etl_functions.py
@@ -2,76 +2,63 @@ import configparser
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import datetime as dt
+import os
+# import warnings
+# warnings.filterwarnings("ignore")
 
-config = configparser.ConfigParser()
-config.read("../src/config.ini")
+import numpy as np
 
-output = config['paths']['data_path']
+# For vizualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import plot_confusion_matrix
 
-trn_lbls_path = config['paths']['train_labels']
-trn_df_path = config['paths']['train_data']
-tst_df_path = config['paths']['test_data']
-sub_form_path = config['paths']['sub_form']
+# To create a quick model to look at Feature Importances
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-def simple_clean(df):
-    '''
-    Set 'id' as the index and drop specified columns. 
-    Columns were dropped based on features importance and 
-    correlation.
-    '''
-    df.index = df['id']
-    to_drop = ['id', 'date_recorded', 'scheme_name',
-               'extraction_type', 'extraction_type_group',
-               'management_group',
-               'payment_type',
-               'quantity_group',
-               'source_type','source_class', 
-               'waterpoint_type_group',
-               'district_code', 
-               'construction_year',
-               'num_private',
-               'recorded_by']
-    df.drop(columns=to_drop, inplace=True)
+# To save csv's with current date information
+import datetime as dt
 
-def add_categories(df):
+def simple_clean(train_df, test_df, to_drop):
     '''
-    Add 'Unknown' or 'Other' to columns with category dtype.
-    This allows for imputing nulls.
+    Set 'id' as the index and drop specified columns.
     '''
-    for col in ['funder', 'installer', 'subvillage', 'scheme_management']:
-        if col=='scheme_management':
-            df[col] = df[col].cat.add_categories('Unknown')
-        else:
-            df[col] = df[col].cat.add_categories('Other')
+    train_df.index, test_df.index = train_df['id'], test_df['id']
+    train_df.drop(columns=to_drop, inplace=True)
+    test_df.drop(columns=to_drop, inplace=True)
 
-def fill_nulls(df):
+def fill_nulls(df, fill_dict):
     '''
-    Impute nulls with the specified fillna dictionary.
+    Add fill value to columns with missing data if not included already.
+    Then, fill the df with the corresponding fill value from the fill dictionary.
     '''
-    df.fillna({'funder':'Other',
-                'installer': 'Other',
-                'subvillage': 'Other', 
-                'public_meeting': False,
-                'scheme_management': 'Unknown',
-                'permit': False}
-                , inplace=True)
+    for col in ['funder', 'installer', 'subvillage', 'scheme_management', 'scheme_name']:
+        try:
+                df[col] = df[col].cat.add_categories(fill_dict[col])
+        except:
+            continue
+    df.fillna(fill_dict, inplace=True)
 
-def encode_me(train_df, test_df):
+def transform_data(train_df, test_df, n=20):
     '''
-    Take categorical columns and one hot encode columns.
+    Separates categorical columns and one hot encode columns.
     Return concatenation of numerical columns and encoded columns.
     '''
     trn_cats = train_df.select_dtypes(include='category')
-    trn_nums = train_df.select_dtypes(exclude='category')
     tst_cats = test_df.select_dtypes(include='category')
+    trn_nums = train_df.select_dtypes(exclude='category')
     tst_nums = test_df.select_dtypes(exclude='category')
 
+    trn_cats, test_cats, cat_dict=lower_features(trn_cats, tst_cats, n)
     trn_cats, tst_cats = ohe_me(trn_cats, tst_cats)
 
     train_df = trn_nums.merge(trn_cats, left_index=True, right_index=True, how='inner')
     test_df = tst_nums.merge(tst_cats, left_index=True, right_index=True, how='inner')
 
-    return train_df, test_df
+    return train_df, test_df, cat_dict
 
 def ohe_me(trn_cats, tst_cats):
     '''
@@ -87,97 +74,94 @@ def ohe_me(trn_cats, tst_cats):
                             columns = ohe.get_feature_names(), index = tst_cats.index)
     return trn_ohe, tst_ohe
 
-def lower_features(train_df, test_df, n=20):
+def lower_features(trn_cats, tst_cats, n):
     '''
     Lower categories in columns to the specified number of categories (default=20).
     For the specified columns, it will take the top n categories and re-categorize
     remaining categories to the specified value.
     This step is done to reduce dimensionality.
     '''
-    for col in ['wpt_name', 'subvillage', 'installer', 'ward', 'funder', 'lga']:
-        temp = train_df[col].value_counts().head(n).keys()
-        train_df[col] = train_df[col].apply(lambda x: 'Other' if x not in temp else x)
-        test_df[col] = test_df[col].apply(lambda x: 'Other' if x not in temp else x)
-    cols = ['funder', 'installer', 'wpt_name', 'subvillage', 'lga', 'ward']
-    train_df[cols] = train_df[cols].astype('category')
-    test_df[cols] = test_df[cols].astype('category')
+    cols = trn_cats.columns
+    cat_dict = {}
+    for col in cols:
+        try:
+            temp = trn_cats[col].value_counts().head(n).keys()
+            trn_cats[col] = trn_cats[col].apply(lambda x: 'Other' if x not in temp else x)
+            tst_cats[col] = tst_cats[col].apply(lambda x: 'Other' if x not in temp else x)
+            cat_dict[col] = list(trn_cats[col].cat.categories)
+        except:
+            continue
+    trn_cats[cols] = trn_cats[cols].astype('category')
+    tst_cats[cols] = tst_cats[cols].astype('category')
+    return trn_cats, tst_cats, cat_dict
 
-def save_cleaned_data(train_df, test_df):
+def save_cleaned_data(train_df, test_df, current_time, temp_output):
     '''
     Saves cleaned training and testing set as .pkl files with the current
     date and time. Date and time is in the format of DDMMYY_HHMM, with hours
     and minutes including AM or PM.
     '''
-    current_time = dt.datetime.now().strftime("%d%m%Y_%I%M%p")
-    train_df.to_pickle(output+'train_set_cleaned'+current_time+'.pkl')
-    test_df.to_pickle(output+'test_set_cleaned'+current_time+'.pkl')
-    print(f"Saved time: {current_time}")
+    try:
+        os.makedirs(temp_output)
+    except:
+        pass
+    train_df.to_pickle(temp_output+'train_data.pkl')
+    test_df.to_pickle(temp_output+'test_data.pkl')
 
+def make_notes(current_time, temp_output, dtype_dict, to_drop, fill_dict, n, cat_dict):
+    notes = f"""Associated date and time: {current_time}
+Files saved to {temp_output}\n\n
+dtype_dict:
+{improve_format_dct(dtype_dict)}\n
+drop_list:
+{improve_format_lst(to_drop)}\n
+fill_dict:
+{improve_format_dct(fill_dict)}\n
+Grabbed top {n} features\n\n
+Categorical columns and associated categories:
+{improve_format_dct(cat_dict)}
+"""
+    with open(f"{temp_output}experiment_notes.txt", 'w') as f:
+        f.write(notes)
 
-# Dtype Dictionary used when importing csv files.
-dtype_dict = {'amount_tsh': 'float32',
-            'funder': 'category',
-            'gps_height': 'int16',
-            'installer': 'category',
-            'longitude': 'float16',
-            'latitude': 'float16',
-            'wpt_name': 'category',
-            'num_private': 'int16',
-            'basin': 'category',
-            'subvillage': 'category',
-            'region': 'category',
-            'region_code': 'int8',
-            'district_code': 'int8',
-            'lga': 'category',
-            'ward': 'category',
-            'population': 'int16',
-            'recorded_by': 'category',
-            'scheme_management': 'category',
-            'construction_year': 'int16',
-            'extraction_type': 'category',
-            'extraction_type_group': 'category',
-            'extraction_type_class': 'category',
-            'management': 'category',
-            'management_group': 'category',
-            'payment': 'category',
-            'payment_type': 'category',
-            'water_quality': 'category',
-            'quality_group': 'category',
-            'quantity': 'category',
-            'quantity_group': 'category',
-            'source': 'category',
-            'source_type': 'category',
-            'source_class': 'category',
-            'waterpoint_type': 'category',
-            'waterpoint_type_group': 'category'}
+def improve_format_lst(my_list):
+    temp = ""
+    for l in my_list:
+        temp+=f"\t{l},\n"
+    return temp[:-1]
 
-def get_cleaned_sets(trn_df_path, trn_lbls_path, tst_df_path, sub_form_path, dtype_dict):
-    '''
-    Load and apply cleaning processes to the specified path variables and 
-    dtype dictionary. Will save the cleaned dataframes to the specified 
-    output in config.ini
-    '''
-    # import data
-    train_df = pd.read_csv(trn_df_path, dtype=dtype_dict)
-    train_lbls = pd.read_csv(trn_lbls_path, dtype=dtype_dict)
-    test_df = pd.read_csv(tst_df_path, dtype=dtype_dict)
-    sub_form = pd.read_csv(sub_form_path, dtype=dtype_dict)
-    # remove specified columns + index='id'
-    simple_clean(train_df)
-    simple_clean(test_df)
-    # add categories to specified columns
-    add_categories(train_df)
-    add_categories(test_df)
-    # fill null values with specified dictionary
-    fill_nulls(train_df)
-    fill_nulls(test_df)
-    # set specified columns up to top 25 categories + 'Other'/'Unknown'
-    lower_features(train_df, test_df, 25)
-    # One hot encode categoricals
-    train_df, test_df = encode_me(train_df, test_df)
-    # merge training data and labels
-    train_df = train_df.merge(train_lbls, left_index=True, right_on='id')
-    # save cleaned training and testing sets
-    save_cleaned_data(train_df, test_df)
-    
-get_cleaned_sets(trn_df_path, trn_lbls_path, tst_df_path, sub_form_path, dtype_dict)
+def improve_format_dct(my_dict):
+    temp = ""
+    for k,v in my_dict.items():
+        temp += f"\t{k}:{v},\n"
+    return temp[:-1]
+
+def import_me(data_path, dtype_dict=None):
+    if ".csv" in data_path:
+        df = pd.read_csv(data_path, dtype=dtype_dict)
+    else:
+        df = pd.read_pkl(data_path)
+    return df
+
+def get_cleaned_sets(train_df, test_df, to_drop, output, fill_dict, dtype_dict=None, n=20,return_output=False):
+    if isinstance(train_df, str):
+        train_df = import_me(train_df, dtype_dict)
+    if isinstance(test_df, str):
+        test_df = import_me(test_df, dtype_dict)
+
+    current_time = dt.datetime.now().strftime("%d%m%y_%I%M%p")
+    temp_output = f"{output}experiments/{current_time}/"
+
+    simple_clean(train_df, test_df, to_drop)
+    fill_nulls(train_df, fill_dict)
+    fill_nulls(test_df, fill_dict)
+    train_df, test_df, cat_dict = transform_data(train_df, test_df)
+
+    save_cleaned_data(train_df, test_df,current_time, temp_output)
+    make_notes(current_time, temp_output,
+            dtype_dict, to_drop, fill_dict, n, cat_dict)
+
+    print(f"Cleaning successful.\nAssociated time is {current_time}")
+
+    if return_output:
+        return train_df, test_df, temp_output
